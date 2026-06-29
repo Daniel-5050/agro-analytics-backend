be6e69c09b8fc8117148a1fb6417000009186d07
Daniel Damatie <wedudu44@gmail.com>
2026-06-29 14:18:53 +0100

    Minimal Express test

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
-dotenv.config();
-connectDB();
+const express = require("express");
 
 const app = express();
 
-/* =========================
-   MIDDLEWARE
-========================= */
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-/* =========================
-   ROUTES
-========================= */
-app.use('/api/inventory', require('./routes/inventoryRoutes'));
-app.use('/api/weather', require('./routes/weatherRoutes'));
-app.use('/api/market-prices', require('./routes/marketRoutes'));
-
-app.get('/', (req, res) => {
-  res.send('Agro-Analytics Backend Running...');
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
 });
 
-/* =========================
-   SERVER START
-========================= */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
